bd5a9cbcdb37e575fa84ef93d69c840163008a1c
adubnos <gp39tu8526@gmail.com>
2023-12-06 10:07:52 +0900

    Initial commit

---
+++ src/main/java/com/example/pmproject/DTO/MemberUpdateDTO.java
@@ -0,0 +1,26 @@
+package com.example.pmproject.DTO;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberUpdateDTO {
+
+    private String email;
+
+    private String recentPassword;
+
+    @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력하여 주십시오.")
+    private String newPassword;
+
+    private String name;
+
+    private String address;
+
+    private String tel;
+
+}
